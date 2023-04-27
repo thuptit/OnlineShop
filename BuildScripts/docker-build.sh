6793ded85733c4eccf64ec368dde46956deaaaa0
thuptit <thunv.B18CN655@stu.ptit.edu.vn>
2023-04-27 14:12:06 +0700

    debug

--- BuildScripts/docker-build.sh
+++ BuildScripts/docker-build.sh
@@ -10,14 +10,15 @@ echo "Root folder: $ROOT_WORKING_DIR"
 
 for service in "${!MICROSERVICE_LIST[@]}";
 do
-    buildService=$(echo "build_${serivce//./}" | tr "[:lower:]" "[:upper:]")
+    echo "name serivce: $service"
+    # buildService=$(echo "build_${serivce//./}" | tr "[:lower:]" "[:upper:]")
     
-    echo "Folder Service: $ROOT_WORKING_DIR/${MICROSERVICE_LIST[${serivce}]#.\/}"
+    # echo "Folder Service: $ROOT_WORKING_DIR/${MICROSERVICE_LIST[${serivce}]#.\/}"
 
-    cd "$ROOT_WORKING_DIR/${MICROSERVICE_LIST[${serivce}]#.\/}"
-    publishDir="obj/Docker/publish"
+    # cd "$ROOT_WORKING_DIR/${MICROSERVICE_LIST[${serivce}]#.\/}"
+    # publishDir="obj/Docker/publish"
 
-    dotnet pushlish --no-restore --output "$publishDir" > /dev/null
+    # dotnet pushlish --no-restore --output "$publishDir" > /dev/null
 
     cd "$ROOT_WORKING_DIR"
 

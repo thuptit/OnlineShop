8a6f1e523d1970fad5791413260d17cea4456acf
thuptit <thunv.B18CN655@stu.ptit.edu.vn>
2023-04-27 17:16:30 +0700

    push image to registry

--- BuildScripts/docker-build.sh
+++ BuildScripts/docker-build.sh
@@ -34,8 +34,7 @@ do
 
     str_not_slash=${service//\//}
     str_not_dot=${str_not_slash//./}
-    current_datetime=$(date +"%Y%m%d%H%M%S")
-    image_name=${str_not_dot,,}$current_datetime
+    image_name=${str_not_dot,,}
     
     docker build -t "$image_name" . > /dev/null
 
---
+++ BuildScripts/docker-push.sh
@@ -0,0 +1,14 @@
+set -e
+
+source BuildScripts/variables.sh
+
+docker login docker-registry.hopto.org -u Thutrang2709 --password-stdin Thutrang2709
+
+for service in "${MICROSERVICE_LIST[@]}";
+do
+    str_not_slash=${service//\//}
+    str_not_dot=${str_not_slash//./}
+    image_name=${str_not_dot,,}
+    docker tag "$image_name" docker-registry.hopto.org/"$image_name":1.0 > /dev/nul
+    docker push docker-registry.hopto.org/"$image_name":1.0 > /dev/nul
+done

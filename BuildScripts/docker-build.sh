744e18a56bcc026a3b9fa3e86122507735705e36
thuptit <thunv.B18CN655@stu.ptit.edu.vn>
2023-04-27 16:09:07 +0700

    fix build image

--- BuildScripts/docker-build.sh
+++ BuildScripts/docker-build.sh
@@ -34,7 +34,7 @@ do
 
     str_not_slash=${service//\//}
     str_not_dot=${str_not_slash//./}
-    current_datetime=$(date + "%Y%m%d%H%M%S")
+    current_datetime=$(date +"%Y%m%d%H%M%S")
     image_name=$str_not_dot$current_datetime
     
     docker build -t "$image_name" . > /dev/null

cdc70b607face43e0c837bf4998ce0b371b47178
thuptit <thunv.B18CN655@stu.ptit.edu.vn>
2023-04-27 23:32:32 +0700

    update push image

--- BuildScripts/docker-push.sh
+++ BuildScripts/docker-push.sh
@@ -2,13 +2,11 @@ set -e
 
 source BuildScripts/variables.sh
 
-docker login http://docker-registry.hopto.org -u Thutrang2709 -p Thutrang2709
-
 for service in "${MICROSERVICE_LIST[@]}";
 do
     str_not_slash=${service//\//}
     str_not_dot=${str_not_slash//./}
     image_name=${str_not_dot,,}
-    docker tag "$image_name" docker-registry.hopto.org/"$image_name":1.0 > /dev/nul
-    docker push docker-registry.hopto.org/"$image_name":1.0 > /dev/nul
+    docker tag "$image_name" --insecure-registry=103.148.57.56/"$image_name":1.0 > /dev/nul
+    docker push --insecure-registry=103.148.57.56/"$image_name":1.0 > /dev/nul
 done

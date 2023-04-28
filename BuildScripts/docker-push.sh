bc5b1a1ab939497b120381bdf9edb1a47e648fb8
thuptit <thunv.B18CN655@stu.ptit.edu.vn>
2023-04-28 09:49:16 +0700

    fix push image

--- BuildScripts/docker-push.sh
+++ BuildScripts/docker-push.sh
@@ -4,12 +4,14 @@ source BuildScripts/variables.sh
 
 getMicroserviceByFolder .
 
+docker login -u thudevdockerv1 -p 14102000Aa@ 
+
 for service in "${MICROSERVICE_LIST[@]}";
 do
     str_not_slash=${service//\//}
     str_not_dot=${str_not_slash//./}
     image_name=${str_not_dot,,}
     echo "$image_name"
-    docker tag "$image_name" thudevdockerv1"$image_name":1.0 > /dev/nul
+    docker tag "$image_name" thudevdockerv1/"$image_name":1.0 > /dev/nul
     docker push thudevdockerv1/"$image_name":1.0 > /dev/nul
 done
